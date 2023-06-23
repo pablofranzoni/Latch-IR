c8ad63b6695f922673df202223e7dc20bb0707a5
pablofranzoni <pablo.franzoni@gmail.com>
2023-06-23 18:49:08 -0300

    Delete comments

--- ir_prueba1.ino
+++ ir_prueba1.ino
@@ -1,36 +1,10 @@
 /*
- * SimpleReceiver.cpp
+ * ir_prueba1.ino
  *
- * Demonstrates receiving NEC IR codes with IRremote
- *
- *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
- *
- ************************************************************************************
- * MIT License
- *
- * Copyright (c) 2020-2022 Armin Joachimsmeyer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  ************************************************************************************
  */
-#define DECODE_NEC  // Includes Apple and Onkyo
-
+#define DECODE_NEC  
 #include <Arduino.h>
 #include "PinDefinitionsAndMore.h"  // Define macros for input and output pin etc.
 #include <IRremote.hpp>
@@ -50,7 +24,7 @@ void loop() {
 
   if (IrReceiver.decode()) {
     Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);  // Print "old" raw data
-    // USE NEW 3.x FUNCTIONS
+    
     IrReceiver.printIRResultShort(&Serial);  // Print complete received data in one line
     IrReceiver.printIRSendUsage(&Serial);    // Print the statement required to send this data
 
@@ -59,7 +33,6 @@ void loop() {
       currentIndex = 0;
     }
 
-    // Obtener el tiempo actual
     unsigned long actualTime = millis();
 
     // Verificar si ha pasado suficiente tiempo desde la última repetición
